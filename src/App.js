a74314b40ec2aea5dbf1b72ab4dac62c086fe26e
GiovannaScuotto <scuottogiovanna@gmail.com>
2025-03-10 03:24:16 +0100

    add Home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import backgroundImage from './images/background.jpeg';
 import HomePage from './HomePage';
 import Page1 from './Page1';
 import Page2 from './Page2';
+import Page3 from './Page3';  // Aggiunto per la pagina "Vedi Tutto"
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
-
-  const goToHome = () => setCurrentPage('home');
-  const goToPage1 = () => setCurrentPage('page1');
-  const goToPage2 = () => setCurrentPage('page2');
 
   return (
     <div style={{
@@ -21,19 +18,14 @@ function App() {
       width: "100vw",
       margin: 0,
       padding: 0,
-      overflow: 'hidden'
+      overflowY: 'auto'
     }}>
 
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <button onClick={goToHome} style={{ margin: "10px", padding: "10px 20px" }}>Home</button>
-        <button onClick={goToPage1} style={{ margin: "10px", padding: "10px 20px" }}>Page 1</button>
-        <button onClick={goToPage2} style={{ margin: "10px", padding: "10px 20px" }}>Page 2</button>
-      </div>
-
       <div style={{ textAlign: "center", marginTop: "50px", color: "white" }}>
-        {currentPage === 'home' && <HomePage />}
+      {currentPage === 'home' && <HomePage setCurrentPage={setCurrentPage} />}
         {currentPage === 'page1' && <Page1 />}
         {currentPage === 'page2' && <Page2 />}
+        {currentPage === 'page3' && <Page3 />}
       </div>
     </div>
   );
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,21 @@
-// src/HomePage.js
-import React from 'react';
+import React from "react";
+import "./App.css";
+import hkheart from './images/heart.png';
 
-function HomePage() {
-  return <h1>Home Page</h1>;
+function HomePage({ setCurrentPage }) {
+  return (
+    <div className="container">
+      {/* Pulsante per tornare indietro alla Home */}
+      <button className="go-button" onClick={() => setCurrentPage('page1')}>
+      AVANTI ⭢
+      </button>
+      <div className="firsthome-box">
+    Benvenuto/a su
+    <img src={hkheart} alt="HK" width="100%" height="auto" />
+    <h1>Hacked Heart</h1>
+    <info>Dove i segreti sono la quotidianità e la fiducia è la prima cosa che perdi.</info>
+    </div>
+    </div>
+  );
 }
-
 export default HomePage;
