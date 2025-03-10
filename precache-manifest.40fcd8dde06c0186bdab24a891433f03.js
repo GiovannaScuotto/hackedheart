0e1c46fce47f3f69e0f46ccbfa014e670cc19845
GiovannaScuotto <scuottogiovanna@gmail.com>
2025-03-10 15:07:27 +0100

    Updates

---
+++ precache-manifest.40fcd8dde06c0186bdab24a891433f03.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b37ffdc55dba9db63d1a01955db4948",
+    "url": "/hackedheart/index.html"
+  },
+  {
+    "revision": "682c4cd39e7410017c6a",
+    "url": "/hackedheart/static/css/main.6a1be0ab.chunk.css"
+  },
+  {
+    "revision": "e3fd08eaf33e511ae61a",
+    "url": "/hackedheart/static/js/2.a81157c1.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/hackedheart/static/js/2.a81157c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e929b384082eb44a4a",
+    "url": "/hackedheart/static/js/3.48ae9480.chunk.js"
+  },
+  {
+    "revision": "682c4cd39e7410017c6a",
+    "url": "/hackedheart/static/js/main.cb8919c5.chunk.js"
+  },
+  {
+    "revision": "a70f6cf15efed4ffa588",
+    "url": "/hackedheart/static/js/runtime-main.e30af246.js"
+  },
+  {
+    "revision": "eef3f426c579bfdfa89adaec3d67631e",
+    "url": "/hackedheart/static/media/background.eef3f426.jpeg"
+  },
+  {
+    "revision": "a2843b394ccfbb5b76688c9ae770de5d",
+    "url": "/hackedheart/static/media/heart.a2843b39.png"
+  },
+  {
+    "revision": "b3f49a157a713f0bb57e4ba969487ed6",
+    "url": "/hackedheart/static/media/member (1).b3f49a15.jpg"
+  },
+  {
+    "revision": "a5eed6dbf48a5a7be367fffc3d570762",
+    "url": "/hackedheart/static/media/member (10).a5eed6db.jpg"
+  },
+  {
+    "revision": "634c1815219656a07769bbf3c78a5c91",
+    "url": "/hackedheart/static/media/member (11).634c1815.jpg"
+  },
+  {
+    "revision": "66386085efed702ef92d61ed22450a66",
+    "url": "/hackedheart/static/media/member (12).66386085.jpg"
+  },
+  {
+    "revision": "ad7dcebc091cb9fe0f2306637390989b",
+    "url": "/hackedheart/static/media/member (13).ad7dcebc.jpg"
+  },
+  {
+    "revision": "724b9086ef92ef608e779b226e98ba58",
+    "url": "/hackedheart/static/media/member (14).724b9086.jpg"
+  },
+  {
+    "revision": "02476d04303a2c927a0fdcbbfb6b9847",
+    "url": "/hackedheart/static/media/member (15).02476d04.jpg"
+  },
+  {
+    "revision": "436ac459ed8316bdeb2f7925cdccd347",
+    "url": "/hackedheart/static/media/member (16).436ac459.jpg"
+  },
+  {
+    "revision": "95bed120d08b0a8446dcc805b019c154",
+    "url": "/hackedheart/static/media/member (2).95bed120.jpg"
+  },
+  {
+    "revision": "382c84b953e89f50b8f67b165c566ad7",
+    "url": "/hackedheart/static/media/member (3).382c84b9.jpg"
+  },
+  {
+    "revision": "7ac744f100fa3765fec4b678ac9176f7",
+    "url": "/hackedheart/static/media/member (4).7ac744f1.jpg"
+  },
+  {
+    "revision": "58f4e4834e291d3c1c71f999ebe8d8bb",
+    "url": "/hackedheart/static/media/member (5).58f4e483.jpg"
+  },
+  {
+    "revision": "33f38994523c123166e553f9befb86a8",
+    "url": "/hackedheart/static/media/member (6).33f38994.jpg"
+  },
+  {
+    "revision": "f4d3d1a8cf9f53971fdfdb08370de050",
+    "url": "/hackedheart/static/media/member (7).f4d3d1a8.jpg"
+  },
+  {
+    "revision": "c77f61f0fd902802c146ae2466cbf585",
+    "url": "/hackedheart/static/media/member (8).c77f61f0.jpg"
+  },
+  {
+    "revision": "4b0a435e78c3400176ae8569b0bdbfde",
+    "url": "/hackedheart/static/media/member (9).4b0a435e.jpg"
+  },
+  {
+    "revision": "2ff4fd08f8c2630c7f6ab328bae5f233",
+    "url": "/hackedheart/static/media/sensitive.2ff4fd08.png"
+  },
+  {
+    "revision": "63920ad3feb23354f2912c982a3efcaa",
+    "url": "/hackedheart/static/media/user.63920ad3.png"
+  }
+]);
